033f6e0ee863e152c72e8aaa8aa814b8d1b96172
Samuel Udoh <samudoh93@gmail.com>
2018-09-19 07:02:28 +0100

    updated

--- module-sec-group/main.tf
+++ module-sec-group/main.tf
@@ -9,9 +9,8 @@ resource "aws_security_group_rule" "allow_all" {
   type            = "ingress"
   from_port       = 443
   to_port         = 443
-  protocol        = "https"
+  protocol        = "tcp"
   cidr_blocks     = ["86.180.132.162/32"]
-
-  security_group_id = "aws_security_group.https.id"
-
+  security_group_id = "sg-0a5f93d20656c663a"
+  depends_on = ["aws_security_group.https"]
   }
